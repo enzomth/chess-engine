1fe4905babcfa60ef99b3c1349f884a5686b669b
Isidore GOMENDY <isidore.gomendy@insa-rennes.fr>
2025-03-14 15:23:44 +0100

    ajout alpha beta pruning

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "map"
 
 using namespace chess;
-#define DEPTH 5
+#define DEPTH 6
 std::map<chess::PieceType, int> piece_values = {
     {chess::PieceType::PAWN, 10},
     {chess::PieceType::KNIGHT, 30},
@@ -13,7 +13,10 @@ std::map<chess::PieceType, int> piece_values = {
     {chess::PieceType::KING, 1000}
 };
 
+int nbPos = 0;
+
 int evaluate_move(const chess::Board &board, const chess::Move &move) {
+    nbPos++;
     chess::Board newBoard = board;
     newBoard.makeMove(move);
     std::pair<chess::GameResultReason, chess::GameResult> result = newBoard.isGameOver();
@@ -27,6 +30,10 @@ int evaluate_move(const chess::Board &board, const chess::Move &move) {
         int value = piece_values[captured_piece.type()];
         return value;
     }
+    return 0;
+}
+
+int evaluate(Board b) {
     return 0;
 }
 
@@ -67,6 +74,48 @@ int minmax(chess::Board board, int depth, bool isMaximizingPlayer){
 
 }
 
+int minmaxAlphaBeta(chess::Board board, int depth, bool isMaximizingPlayer,int alpha, int beta) {
+    if (depth == 0) {
+        return evaluate(board);  //on s'arrete ici
+    }
+
+    Movelist moves;
+    movegen::legalmoves(moves, board);
+    if (moves.empty()) {
+        return 0;  // Pas de coups possibles : Mat
+    }
+
+    if (isMaximizingPlayer) {
+        int maxEval = std::numeric_limits<int>::min();
+        for (const auto &move : moves) {
+            chess::Board newBoard = board;
+            newBoard.makeMove(move);  // Joue le coup
+            int eval = minmaxAlphaBeta(newBoard, depth - 1, false, alpha, beta)-depth;
+            maxEval = std::max(maxEval, eval);
+            alpha = std::max(alpha, maxEval);
+            if (beta <= alpha) {
+                break;
+            }
+        }
+        return maxEval;
+    }
+
+    else {
+        int minEval = std::numeric_limits<int>::max();
+        for (const auto &move : moves) {
+            chess::Board newBoard = board;
+            newBoard.makeMove(move);  // Joue le coup
+            int eval = minmaxAlphaBeta(newBoard, depth - 1, true,alpha,beta);
+            minEval = std::min(minEval, eval);
+            beta = std::min(beta, minEval);
+            if (beta <= alpha) {
+                break;
+            }
+        }
+        return minEval;
+    }
+}
+
 chess::Move best_move(chess::Board &board, int depth) {
     Movelist moves;
     movegen::legalmoves(moves, board);
@@ -84,7 +133,7 @@ chess::Move best_move(chess::Board &board, int depth) {
         chess::Board newBoard = board;
         newBoard.makeMove(move);
         
-        int eval = evaluate_move(board,move) + minmax(newBoard, depth - 1, false); // Appel MinMax pour évaluer ce coup
+        int eval = evaluate_move(board,move) + minmaxAlphaBeta(newBoard, depth - 1, false,std::numeric_limits<int>::min(),std::numeric_limits<int>::max()); // Appel MinMax pour évaluer ce coup
         std::cout << eval<< " : " << uci::moveToUci(move) <<std::endl;
 
         if (eval > best_eval) {
@@ -97,15 +146,18 @@ chess::Move best_move(chess::Board &board, int depth) {
 }
 
 int main () {
-    Board board = Board("k7/1R6/8/8/7R/8/8/K7 w - - 0 1");
+    //Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq – 0 1");
+    Board board = Board("3r1r2/1k1q3p/p5p1/1pQ3P1/8/PBRn4/1P5b/K3R3 w - - 0 1");
 
-    for(int i=0; i<20; i++){
-    chess::Move move = best_move(board,DEPTH);
-    std::cout << "Meilleur coup : " << uci::moveToUci(move) << std::endl;
+    for(int i=0; i<5; i++){
+        chess::Move move = best_move(board,DEPTH);
+        std::cout << "Meilleur coup : " << uci::moveToUci(move) << std::endl;
 
-    board.makeMove(move);
-}
-std::cout << "FEN  : " << board.getFen() << std::endl;
+        board.makeMove(move);
+    }
+
+    std::cout << "nb pos : "<<nbPos << std::endl;
+    std::cout << "FEN  : " << board.getFen() << std::endl;
 
     return 0;
 }
