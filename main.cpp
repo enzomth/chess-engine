b43a9c315161202879ea4975c131c412f2a9d183
Isidore GOMENDY <isidore.gomendy@insa-rennes.fr>
2025-03-21 17:01:39 +0100

    avant de pull

--- main.cpp
+++ main.cpp
@@ -266,7 +266,7 @@ int main () {
     Board board = Board("8/P1Q1nk1p/5p2/4p3/4P2p/6P1/r4PKP/3q4 w - - 0 1");
 
     for(int i=0; i<1; i++){
-        chess::Move move = best_move_iterative_deepening(board,5000);
+        chess::Move move = best_move_iterative_deepening(board,1000);
         std::cout << "Meilleur coup : " << uci::moveToUci(move) << std::endl;
 
         board.makeMove(move);
